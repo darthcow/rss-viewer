1e97f1f676402f42a96823c770f14d8cf5d45760
julio_cesar_oliveira <julio_cesar-almeida@hotmail.com>
2019-10-28 12:38:51 -0300

    made DownloadData Class public for viewmodel to use it

--- RSSDownloader/app/src/main/java/com/project/rssdownloader/DownloadData.kt
+++ RSSDownloader/app/src/main/java/com/project/rssdownloader/DownloadData.kt
@@ -15,7 +15,7 @@ import java.lang.Exception
 import java.net.URL
 import kotlin.properties.Delegates
 
-private class DownloadData(private val callBack: DownloaderCallBack) :
+class DownloadData(private val callBack: DownloaderCallBack) :
     AsyncTask<String, Void, String>() {
     var mContext: Context by Delegates.notNull()
     var mListView: ListView by Delegates.notNull()
@@ -24,10 +24,7 @@ private class DownloadData(private val callBack: DownloaderCallBack) :
         fun onDataAvailable(data: List<FeedEntry>)
     }
 
-
-
     override fun doInBackground(vararg p0: String?): String {
-        Log.d("", "doInBackground called starts with ${p0[0]}")
         val feed = downloadXML(p0[0])
         if (feed.isEmpty()) {
             Log.d("", "error downloading feed")
